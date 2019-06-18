10c18e6e1360b5ca22d465985c8140d2b5a9060e
Gergely Kocsis <koger23@gmail.com>
2019-06-18 16:04:02 +0200

    feat(AuthController): add decryption of login datas

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -18,18 +18,30 @@ namespace myRestAPI.Controllers
         [HttpPost("token")]
         public IActionResult Token()
         {
-            var claimsData = new[] { new Claim(ClaimTypes.Name, "username") };
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisanotsecuresecuritykey"));
-            var signInCreditentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-            var token = new JwtSecurityToken(
-                issuer:"example.com",
-                audience:"example.com",
-                expires:DateTime.Now.AddMinutes(1),
-                claims:claimsData,
-                signingCredentials:signInCreditentials
-                );
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            return Ok(tokenString);
+            var header = Request.Headers["Authorization"];
+            if (header.ToString().StartsWith("Basic"))
+            {
+                var creditentialValue = header.ToString().Substring("Basic ".Length).Trim();
+                var usernameAndPasswordString = Encoding.UTF8.GetString(Convert.FromBase64String(creditentialValue));
+                var usernameAndPassowrd = usernameAndPasswordString.Split(":");
+                // here to check in database the username and password
+                if (usernameAndPassowrd[0] == "admin" && usernameAndPassowrd[1] == "password")
+                {
+                    var claimsData = new[] { new Claim(ClaimTypes.Name, usernameAndPassowrd[0]) };
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisanotsecuresecuritykey"));
+                    var signInCreditentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                    var token = new JwtSecurityToken(
+                        issuer: "example.com",
+                        audience: "example.com",
+                        expires: DateTime.Now.AddMinutes(1),
+                        claims: claimsData,
+                        signingCredentials: signInCreditentials
+                        );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+            }
+            return BadRequest();
         }
     }
 }
