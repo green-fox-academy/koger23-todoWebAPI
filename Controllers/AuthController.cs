9033f6599dd8135449ed85c41fef2cdf24b6778d
Gergely Kocsis <koger23@gmail.com>
2019-06-18 17:18:37 +0200

    refactor(AuthController): move check methods to AuthService
    Implement AuthService with DI

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Mvc;
+using myRestAPI.Services;
 
 namespace myRestAPI.Controllers
 {
@@ -15,31 +7,20 @@ namespace myRestAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private AuthService authService;
+
+        public AuthController(AuthService authService)
+        {
+            this.authService = authService;
+        }
+
         [HttpPost("token")]
         public IActionResult Token()
         {
-            var header = Request.Headers["Authorization"];
-            if (header.ToString().StartsWith("Basic"))
+            string result = authService.checkToken(Request.Headers["Authorization"]);
+            if (result != null)
             {
-                var creditentialValue = header.ToString().Substring("Basic ".Length).Trim();
-                var usernameAndPasswordString = Encoding.UTF8.GetString(Convert.FromBase64String(creditentialValue));
-                var usernameAndPassowrd = usernameAndPasswordString.Split(":");
-                // here to check in database the username and password
-                if (usernameAndPassowrd[0] == "admin" && usernameAndPassowrd[1] == "password")
-                {
-                    var claimsData = new[] { new Claim(ClaimTypes.Name, usernameAndPassowrd[0]) };
-                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisanotsecuresecuritykey"));
-                    var signInCreditentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-                    var token = new JwtSecurityToken(
-                        issuer: "example.com",
-                        audience: "example.com",
-                        expires: DateTime.Now.AddMinutes(1),
-                        claims: claimsData,
-                        signingCredentials: signInCreditentials
-                        );
-                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-                    return Ok(tokenString);
-                }
+                return Ok(result);
             }
             return Unauthorized();
         }
