f9e7eff0fdc121eb10f52ae7958f44788778410c
Gergely Kocsis <koger23@gmail.com>
2019-06-18 12:42:36 +0200

    first commit

---
+++ Models/Assignee/Assignee.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace myRestAPI.Models
+{
+    public class Assignee
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public ICollection<Todo> Student { get; set; }
+    }
+}
---
+++ Repositories/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using myRestAPI.Models;
+
+namespace myRestAPI
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Assignee> Assignees { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
