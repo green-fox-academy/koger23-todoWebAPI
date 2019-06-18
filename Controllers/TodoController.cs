601f0e71b9dc4ac80c0f604e425892a691ca7d86
Gergely Kocsis <koger23@gmail.com>
2019-06-18 16:31:22 +0200

    feat(TodoController): make all endpoints to be authorized

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using myRestAPI.Models;
@@ -9,6 +10,7 @@ using myRestAPI.Services;
 
 namespace myRestAPI.Controllers
 {
+    [Authorize]
     [Route("api/v1/[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
