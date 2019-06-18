c948e8167be96cd0140a6764132bc9ca8c73c2d7
Gergely Kocsis <koger23@gmail.com>
2019-06-18 16:32:26 +0200

    feat(Startup): add basic token validation

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace myRestAPI
@@ -30,29 +33,45 @@ namespace myRestAPI
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
                 .AddJsonFile("appsettings.json")
                 .Build();
-            services.AddDbContext<ApplicationContext>(build => {
-                build.UseSqlServer(configuration.GetConnectionString(Environment.GetEnvironmentVariable("MSSQL_DB_CONNECTION_STRING")));
+            // String connectionString = Environment.GetEnvironmentVariable("MSSQL_DB_CONNECTION_STRING");
+            String connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=TodoApiDBs;Trusted_Connection=True;MultipleActiveResultSets=True";
+            services.AddDbContext<ApplicationContext>(build =>
+            {
+                build.UseSqlServer(connectionString);
             });
             services.AddScoped(typeof(Services.TodoService), typeof(Services.TodoService));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = "example.com",
+                    ValidAudience = "example.com",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisanotsecuresecuritykey")),
+            };
+        });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+        app.UseAuthentication();
+        app.UseHttpsRedirection();
+        app.UseMvc();
+    }
+}
 }
