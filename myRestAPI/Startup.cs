297048067b9a48128ac2d5f6312abb6e0f656262
Gergely Kocsis <koger23@gmail.com>
2019-06-19 14:39:59 +0200

    fix(Startup): change service registration of TodoService

--- myRestAPI/Startup.cs
+++ myRestAPI/Startup.cs
@@ -6,8 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using myRestAPI.Services;
 using System;
-using System.IO;
 using System.Text;
 
 
@@ -25,19 +25,19 @@ namespace myRestAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddEnvironmentVariables()
-                .AddJsonFile("appsettings.json")
-                .Build();
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddEnvironmentVariables()
+            //    .AddJsonFile("appsettings.json")
+            //    .Build();
             // String connectionString = Environment.GetEnvironmentVariable("MSSQL_DB_CONNECTION_STRING");
             String connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=TodoApiDBs;Trusted_Connection=True;MultipleActiveResultSets=True";
             services.AddDbContext<ApplicationContext>(build =>
             {
                 build.UseSqlServer(connectionString);
             });
-            services.AddScoped(typeof(Services.TodoService), typeof(Services.TodoService));
-            services.AddScoped(typeof(Services.AuthService), typeof(Services.AuthService));
+            services.AddScoped<ITodoService, TodoService>();
+            services.AddScoped(typeof(AuthService), typeof(AuthService));
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
