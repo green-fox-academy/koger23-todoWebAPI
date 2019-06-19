b6953457d96af9cffa1be3b7e9c775557f747f27
Gergely Kocsis <koger23@gmail.com>
2019-06-19 13:10:47 +0200

    feat(Todo): add boolean for state, remove contructors

--- Models/Todo/Todo.cs
+++ Models/Todo/Todo.cs
@@ -11,28 +11,6 @@ namespace myRestAPI.Models
         public string name { get; set; }
         public Assignee assignee { get; set; }
         public string description { get; set; }
-        public Boolean done { get; set; }
-
-        public Todo()
-        {
-            this.done = false;
-        }
-
-        public Todo(string name, Assignee assignee, string description)
-        {
-            this.name = name;
-            this.assignee = assignee;
-            this.description = description;
-            this.done = false;
-        }
-
-        public Todo(long id, string name, Assignee assignee, string description, bool done)
-        {
-            this.id = id;
-            this.name = name;
-            this.assignee = assignee;
-            this.description = description;
-            this.done = done;
-        }
+        public bool done { get; set; }
     }
 }
