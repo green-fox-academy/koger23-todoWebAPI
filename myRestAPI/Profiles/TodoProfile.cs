171fb286582073c232236d9aa3c3de3fc6f2bab1
Gergely Kocsis <koger23@gmail.com>
2019-06-19 15:49:10 +0200

    feat(TodoProfile): create profile for AutoMapper

---
+++ myRestAPI/Profiles/TodoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using myRestAPI.Models;
+
+namespace myRestAPI.Profiles
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<TodoDTO, Todo>();
+            CreateMap<Todo, TodoDTO>();
+        }
+    }
+}
