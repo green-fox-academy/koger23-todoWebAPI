bdcd998c46e3bcec29bf0e04dd1ac9146a7e9086
Gergely Kocsis <koger23@gmail.com>
2019-06-19 13:08:19 +0200

    feat(ITodoService): create interface for todoService

---
+++ Services/ITodoService.cs
@@ -0,0 +1,15 @@
+﻿using myRestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myRestAPI.Services
+{
+    public interface ITodoService
+    {
+        TodoListDTO FindAll();
+        void createTodo(TodoDTO todoDTO);
+        Todo TodoDTOConverter(TodoDTO todoDTO);
+    }
+}
