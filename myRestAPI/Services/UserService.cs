499398f083c021e8edcd10fa3835d600f12d0e8d
Gergely Kocsis <koger23@gmail.com>
2019-06-20 16:18:10 +0200

    feat(UserService): create service

---
+++ myRestAPI/Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myRestAPI.Services
+{
+    public class UserService
+    {
+    }
+}
