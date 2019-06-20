cd4b8bc37be7c655e5efb15b34944527e062a6b4
Gergely Kocsis <koger23@gmail.com>
2019-06-20 16:17:47 +0200

    feat(IUserService): create interface

---
+++ myRestAPI/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using myRestAPI.Models.User;
+using System.Collections.Generic;
+
+namespace myRestAPI.Services
+{
+    interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User userParam, string password = null);
+        void Delete(int id);
+    }
+}
