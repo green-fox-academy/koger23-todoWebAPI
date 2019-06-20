696e6e458972b7b18b2b168c3e61eeca81eb6544
Gergely Kocsis <koger23@gmail.com>
2019-06-20 20:38:43 +0200

    feat(UserController): create endpoints for user
    Registration and Authentication

---
+++ myRestAPI/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using myRestAPI.Helpers;
+using myRestAPI.Models.User;
+using myRestAPI.Services;
+using System.Collections.Generic;
+
+namespace myRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        private IMapper _mapper;
+        private IAuthService _authService;
+
+        public UserController(IUserService userService, IMapper mapper, IAuthService authService)
+        {
+            _userService = userService;
+            _mapper = mapper;
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]UserDTO userDto)
+        {
+            // map dto to entity
+            var user = _mapper.Map<User>(userDto);
+
+            try
+            {
+                // save 
+                _userService.Create(user, userDto.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate()
+        {
+            return _authService.checkToken(Request.Headers["Authorization"]);
+        }
+            
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            var userDtos = _mapper.Map<IList<UserDTO>>(users);
+            return Ok(userDtos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetById(id);
+            var userDto = _mapper.Map<UserDTO>(user);
+            return Ok(userDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]UserDTO userDto)
+        {
+            // map dto to entity and set id
+            var user = _mapper.Map<User>(userDto);
+            user.Id = id;
+
+            try
+            {
+                // save 
+                _userService.Update(user, userDto.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.Delete(id);
+            return Ok();
+        }
+    }
+}
