de863e342199ef670f12f7565bd32c0c4845c8ac
Gergely Kocsis <koger23@gmail.com>
2019-06-20 15:42:58 +0200

    feat(IAuthSerivce): create interface

---
+++ myRestAPI/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myRestAPI.Services
+{
+    interface IAuthService
+    {
+        String checkToken(StringValues header);
+        bool checkUserNameAndPassword(String[] usernameAndPassowrd);
+    }
+}
