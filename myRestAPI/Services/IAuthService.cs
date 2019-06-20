2a7c57552192a49f94be641dcdae764c917cd18f
Gergely Kocsis <koger23@gmail.com>
2019-06-20 15:46:26 +0200

    refactor(IAuthService): remove unused usings

--- myRestAPI/Services/IAuthService.cs
+++ myRestAPI/Services/IAuthService.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.Extensions.Primitives;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace myRestAPI.Services
 {
     interface IAuthService
     {
-        String checkToken(StringValues header);
+        string checkToken(StringValues header);
         bool checkUserNameAndPassword(String[] usernameAndPassowrd);
     }
 }
