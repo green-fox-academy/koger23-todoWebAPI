b1921ec4520bbc5af24a7c03de78e9a503f4f12e
Gergely Kocsis <koger23@gmail.com>
2019-06-21 17:33:35 +0200

    feat: update database

--- myRestAPI/Migrations/20190621143041_AddCreatorForAssignee.Designer.cs
+++ myRestAPI/Migrations/20190621143041_AddCreatorForAssignee.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myRestAPI;
 
 namespace myRestAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190621143041_AddCreatorForAssignee")]
+    partial class AddCreatorForAssignee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ myRestAPI/Models/User/UserSimplified.cs
@@ -0,0 +1,9 @@
+﻿namespace myRestAPI.Models.User
+{
+    public class UserSimplified
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+    }
+}
