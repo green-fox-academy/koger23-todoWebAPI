14da5b94b0ce07b714b4e0d95cbaab9e9621c345
Gergely Kocsis <koger23@gmail.com>
2019-06-21 17:28:06 +0200

    feat(IAssigneeService): create interface

---
+++ myRestAPI/Services/IAssigneeService.cs
@@ -0,0 +1,10 @@
+﻿using myRestAPI.Models;
+using System.Collections.Generic;
+
+namespace myRestAPI.Services
+{
+    public interface IAssigneeService
+    {
+        List<Assignee> GetAssignees();
+    }
+}
