b3e89a127dde4ae7cd416b9a51156e6f776acf99
Gergely Kocsis <koger23@gmail.com>
2019-06-21 09:21:01 +0200

    feat(User): add new properties
    Token, Role

--- myRestAPI/Models/User/User.cs
+++ myRestAPI/Models/User/User.cs
@@ -8,5 +8,7 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
     }
 }
