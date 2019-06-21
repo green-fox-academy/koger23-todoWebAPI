ad2ce593b26ed72ff4913ef56ad784171b886702
Gergely Kocsis <koger23@gmail.com>
2019-06-21 10:46:39 +0200

    feat: update database with new columns of User

---
+++ myRestAPI/Migrations/20190621075945_AddRoleAndTokenToUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace myRestAPI.Migrations
+{
+    public partial class AddRoleAndTokenToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "Users");
+        }
+    }
+}
