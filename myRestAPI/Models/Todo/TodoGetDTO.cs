25267c644226bceb6050d2f4d049dc9635bf3447
Gergely Kocsis <koger23@gmail.com>
2019-06-21 21:52:53 +0200

    feat(TodoGetDTO): create new DTO
    Created to get short informations about DTO without related model properties

---
+++ myRestAPI/Models/Todo/TodoGetDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace myRestAPI.Models
+{
+    public class TodoGetDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long AssigneeId { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public long UserId { get; set; }
+
+        public TodoGetDTO()
+        {
+        }
+    }
+}
