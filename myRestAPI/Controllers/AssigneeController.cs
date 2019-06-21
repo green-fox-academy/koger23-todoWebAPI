9e52aac97bc1eedfb52a4946510b3aa0b15638dd
Gergely Kocsis <koger23@gmail.com>
2019-06-21 17:30:59 +0200

    feat(AssigneeController): create root getter endpoint

--- myRestAPI/Controllers/AssigneeController.cs
+++ myRestAPI/Controllers/AssigneeController.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using myRestAPI.Models;
+using myRestAPI.Services;
+using System.Collections.Generic;
 
 namespace myRestAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AssigneeController : ControllerBase
     {
+        private readonly IAssigneeService assigneeService;
+
+        public AssigneeController(IAssigneeService assigneeService)
+        {
+            this.assigneeService = assigneeService;
+        }
+
+        [HttpGet]
+        public List<Assignee> GetAssignees()
+        {
+            return assigneeService.GetAssignees();
+        }
     }
 }
