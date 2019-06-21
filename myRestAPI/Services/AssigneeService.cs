adfb390aa5a49296ccfa18909cb48a320bc23e43
Gergely Kocsis <koger23@gmail.com>
2019-06-21 17:28:32 +0200

    feat(AssigneeService): create service with implemented interface

---
+++ myRestAPI/Services/AssigneeService.cs
@@ -0,0 +1,21 @@
+﻿using myRestAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myRestAPI.Services
+{
+    public class AssigneeService : IAssigneeService
+    {
+        private readonly ApplicationContext _context;
+
+        public AssigneeService(ApplicationContext context)
+        {
+            _context = context;
+        }
+       
+        public List<Assignee> GetAssignees()
+        {
+            return _context.Assignees.ToList();
+        }
+    }
+}
