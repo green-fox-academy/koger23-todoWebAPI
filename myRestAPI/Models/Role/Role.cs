7d26ce11d88ac05a9aa09de357a0f34bc68ef044
Gergely Kocsis <koger23@gmail.com>
2019-06-21 10:41:30 +0200

    fix(Role): remove unused usings

--- myRestAPI/Models/Role/Role.cs
+++ myRestAPI/Models/Role/Role.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace myRestAPI.Models.Role
+﻿namespace myRestAPI.Models.Role
 {
     public static class Role
     {
