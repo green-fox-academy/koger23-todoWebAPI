7c778606a7c76331ccf37901e29b00030b9833aa
Gergely Kocsis <koger23@gmail.com>
2019-06-22 17:54:28 +0200

    feat(IAssigneeService): add CreateAssignees()
    Changed GetAssignees return type and method body

--- myRestAPI/Services/AssigneeService.cs
+++ myRestAPI/Services/AssigneeService.cs
@@ -1,21 +1,70 @@
-﻿using myRestAPI.Models;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using myRestAPI.Models.Assignee;
+using myRestAPI.Models.Role;
+using myRestAPI.Models.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace myRestAPI.Services
 {
     public class AssigneeService : IAssigneeService
     {
         private readonly ApplicationContext _context;
+        private readonly IMapper _mapper;
 
-        public AssigneeService(ApplicationContext context)
+        public AssigneeService(ApplicationContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-       
-        public List<Assignee> GetAssignees()
+
+        public async Task<ActionResult> CreateAssignees(AssigneeDTO assigneeDTO, int id)
         {
-            return _context.Assignees.ToList();
+            Assignee assignee = _mapper.Map<AssigneeDTO, Assignee>(assigneeDTO);
+            User user = _context.Users.Find(id);
+            assignee.Creator = user;
+            assignee.CreatorId = user.Id;
+            List<Assignee> assignees = _context.Assignees
+                .Where(a => a.CreatorId == user.Id)
+                .Where(a => a.Name == assigneeDTO.Name)
+                .ToList();
+            if (assignees.Count == 0)
+            {
+                _context.Add(assignee);
+                await _context.SaveChangesAsync();
+                return new OkResult();
+            }
+            return new BadRequestResult();
+        }
+
+        public List<AssigneeGetDTO> GetAssignees(int userId)
+        {
+            var assignees = new List<Assignee>();
+            var assigneesDTList = new List<AssigneeGetDTO>();
+            User user = _context.Users.Find(userId);
+            if (user.Role == Role.Admin)
+            {
+                assignees = _context.Assignees
+                    .OrderBy(a => a.Name)
+                    .ToList();
+            } else
+            {
+                assignees = _context.Assignees
+                    .Where(a => a.Creator == user)
+                    .OrderBy(a => a.Name)
+                    .ToList();
+            }
+            if (assignees.Count > 0)
+            {
+                foreach (Assignee assignee in assignees)
+                {
+                    assigneesDTList.Add(_mapper.Map<Assignee, AssigneeGetDTO>(assignee));
+                }
+            }
+            return assigneesDTList;
         }
     }
 }
--- myRestAPI/Services/IAssigneeService.cs
+++ myRestAPI/Services/IAssigneeService.cs
@@ -1,10 +1,13 @@
-﻿using myRestAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using myRestAPI.Models.Assignee;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace myRestAPI.Services
 {
     public interface IAssigneeService
     {
-        List<Assignee> GetAssignees();
+        List<AssigneeGetDTO> GetAssignees(int userId);
+        Task<ActionResult> CreateAssignees(AssigneeDTO assigneeDTO, int userId);
     }
 }
