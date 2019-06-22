0ff3572f6f996b2d28f806a6654c38ef8bdfad57
Gergely Kocsis <koger23@gmail.com>
2019-06-22 19:36:09 +0200

    feat(TodoControllerTest): add new test method for CreateTodo()

--- myRestAPITests/TodoControllerTest.cs
+++ myRestAPITests/TodoControllerTest.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using myRestAPI.Controllers;
 using myRestAPI.Models;
 using myRestAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace myRestAPITests
 {
@@ -14,17 +19,43 @@ namespace myRestAPITests
         {
             var mockTodoService = new Mock<ITodoService>();
 
-            TodoListDTO todoListDTO = new TodoListDTO();
+            var todoListDTO = new List<TodoGetDTO>();
             mockTodoService.Setup(x => x.FindAll())
                 .Returns(todoListDTO);
 
             TodoController todoController = new TodoController(mockTodoService.Object);
 
             // Act
-            TodoListDTO actionResult = todoController.Get();
+            List<TodoGetDTO> actionResult = todoController.Get();
 
             // Assert
             Assert.AreEqual(todoListDTO, actionResult);
+        }
+
+        [TestMethod]
+        public void TodoControllerTest_AddTodo()
+        {
+            // Arrange
+            var mockTodoService = new Mock<ITodoService>();
+            TodoDTO todoDTO = new TodoDTO {
+                Name = "test",
+                AssigneeId = 1,
+                Description = "test add todo endpoint",
+                Done = false,
+                UserId = 1
+            };
+            mockTodoService.Setup(x => x.CreateTodo(todoDTO))
+                .ReturnsAsync(new OkResult());
+
+            TodoController todoController = new TodoController(mockTodoService.Object);
+
+            // Act
+            Task<IActionResult> addTodoResult = todoController.Post(todoDTO);
+
+            // Assert
+            var expected = new OkResult();
+            Assert.IsNotNull(addTodoResult);
+            Assert.AreEqual(expected.GetType(), addTodoResult.Result.GetType());
         }
     }
 }
