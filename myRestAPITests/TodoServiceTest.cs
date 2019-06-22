f3435d60290b6be3d138268a7b8b26a66ccc71f9
Gergely Kocsis <koger23@gmail.com>
2019-06-23 00:04:58 +0200

    test(TodoServiceTest): add new test
    Till one test method for FindAll() for todos

---
+++ myRestAPITests/TodoServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using myRestAPI;
+using myRestAPI.Models;
+using myRestAPI.Profiles;
+using myRestAPI.Services;
+using NUnit.Framework;
+
+namespace myRestAPITests
+{
+    [TestClass]
+    public class TodoServiceTest
+    {
+        [TestMethod]
+        public void FindAll()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+            using (var context = new ApplicationContext(options))
+            {
+                context.Todos.Add(new Todo { Name = "testTodo1" });
+                context.Todos.Add(new Todo { Name = "testTodo2" });
+                context.Todos.Add(new Todo { Name = "testTodo3" });
+                context.SaveChanges();
+            }
+
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            var _mapper = mockMapper.CreateMapper();
+
+            var _todoService = new TodoService(new ApplicationContext(options), _mapper);
+
+            // Act
+            var actual = _todoService.FindAll();
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(3, actual.Count);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("testTodo2", actual[1].Name);
+        }
+    }
+}
